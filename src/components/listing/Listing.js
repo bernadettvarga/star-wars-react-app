8a627f3a49fd4611b6b8425091c176f718077983
Bernadett Varga <detty.v@gmail.com>
2021-03-19 16:04:00 +0100

    Set 'entity' state on location change in Listing component

--- src/components/listing/Listing.js
+++ src/components/listing/Listing.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Title from '../common/Title'
 
 export default function Listing () {
+  const [entity, setEntity] = useState(getEntityFromUrl())
+
+  useEffect(function onLocationChange () {
+    setEntity(getEntityFromUrl())
+  }, [window.location.pathname])
+
   return (
-    <Title name='Listing' />
+    <div>
+      <Title name='Listing' />
+      <p>{entity}</p>
+    </div>
   )
+
+  // ***********************************
+
+  function getEntityFromUrl () {
+    const location = window.location.pathname
+    const match = location.match(/\/listing\/(?<entity>[a-z]+)[/]?/)
+    return match.groups.entity
+  }
 }
