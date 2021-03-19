ff98928dc7ab5b870781ff760e5f1a2f149ba014
Bernadett Varga <detty.v@gmail.com>
2021-03-19 20:30:31 +0100

    Add swapi call in Listing component

--- src/components/listing/Listing.js
+++ src/components/listing/Listing.js
@@ -1,17 +1,33 @@
 import { useState, useEffect } from 'react'
 import Title from '../common/Title'
+import swapi from '../../api/swapi'
 
 export default function Listing () {
   const [entity, setEntity] = useState(getEntityFromUrl())
+  const [results, setResults] = useState([])
 
-  useEffect(function onLocationChange () {
+  useEffect(function setEntityonLocationChange () {
     setEntity(getEntityFromUrl())
   }, [window.location.pathname])
 
+  useEffect(function setResultsOnEntityChange () {
+    async function callSwapi () {
+      try {
+        const { data } = await swapi.get(`/${entity}`)
+        setResults(data.results)
+      } catch (err) {
+        // TODO: set an error state
+        console.log(err)
+      }
+    }
+
+    callSwapi()
+  }, [entity])
+
   return (
     <div>
-      <Title name='Listing' />
-      <p>{entity}</p>
+      <Title name={entity} />
+      <p>{results.length}</p>
     </div>
   )
 
@@ -19,6 +35,7 @@ export default function Listing () {
 
   function getEntityFromUrl () {
     const location = window.location.pathname
+    // TODO: specify the regex more to only accept the valid entities
     const match = location.match(/\/listing\/(?<entity>[a-z]+)[/]?/)
     return match.groups.entity
   }
