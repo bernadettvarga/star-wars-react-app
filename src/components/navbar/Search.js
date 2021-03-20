ffc7f059fd6c9e17c22d562d52ea9b9fb6e0a6e6
Bernadett Varga <detty.v@gmail.com>
2021-03-20 22:08:03 +0100

    Make search work

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import { useState } from 'react'
 import { Form, FormControl, Button } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
 
 export default function Search ({ entity }) {
+  const history = useHistory()
+
+  const [query, setQuery] = useState('')
+
   return (
     <Form inline>
-      <FormControl type='text' placeholder={`Search for ${entity}`} className='mr-sm-2' />
-      <Button variant='outline-success'>Search</Button>
+      <FormControl
+        type='text'
+        placeholder={`Search for ${entity}`}
+        className='mr-sm-2'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={(e) => handlePressingEnter(e)}
+      />
+      <Button variant='outline-success' onClick={() => search()}>Search</Button>
     </Form>
   )
+
+  // *******************
+
+  function search () {
+    if (query) {
+      history.push(`/listing/${entity}/?search=${query}`)
+      setQuery('')
+    }
+  }
+
+  function handlePressingEnter (e) {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      search()
+    }
+  }
 }
