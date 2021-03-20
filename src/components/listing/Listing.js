ec2e4554bdaaaff55cbdca0ebd60a7a5b4aad6ab
Bernadett Varga <detty.v@gmail.com>
2021-03-20 14:16:48 +0100

    Refactor setState calls to make it more readable in Listing component

--- src/components/listing/Listing.js
+++ src/components/listing/Listing.js
@@ -14,24 +14,33 @@ export default function Listing (props) {
     getStateFromLocation
   } = props
 
+  const defaultEntity = getEntityFromLocation(window.location)
+  const defaultPage = Number(getPageFromLocation(window.location))
+
   const [results, setResults] = useState([])
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
-  const [entity, setEntity] = useState(getEntityFromLocation(window.location))
-  const [page, setPage] = useState(Number(getPageFromLocation(window.location)))
+  const [entity, setEntity] = useState(defaultEntity)
+  const [page, setPage] = useState(defaultPage)
   const [itemCount, setItemCount] = useState(0)
 
   const history = useHistory()
 
   useEffect(function setEndpointOnFirstRender () {
-    setEndpoint(getEndpointFromLocation(window.location))
+    const endpoint = getEndpointFromLocation(window.location)
+
+    setEndpoint(endpoint)
   }, [])
 
   useEffect(function updateStatesOnLocationChange () {
     return history.listen((location) => {
-      setEndpoint(getEndpointFromLocation(location))
-      setEntity(getEntityFromLocation(location))
-      setPage(Number(getPageFromLocation(location)))
+      const endpoint = getEndpointFromLocation(location)
+      const entity = getEntityFromLocation(location)
+      const page = Number(getPageFromLocation(location))
+
+      setEndpoint(endpoint)
+      setEntity(entity)
+      setPage(page)
     })
   }, [history])
 
