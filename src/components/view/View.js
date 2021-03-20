c59507bd24ba98c6e28529ab523f5841ba3b693e
Bernadett Varga <detty.v@gmail.com>
2021-03-20 18:21:02 +0100

    Add template for loading and error handling in View component

--- src/components/view/View.js
+++ src/components/view/View.js
@@ -35,6 +35,10 @@ export default function View (props) {
   }, [endpoint])
 
   return (
-    <Title name={result.name || result.title} />
+    <div>
+      {(loading) && <p>Loading...</p>}
+      {(!loading && error) && <p>Oops, an error occured.</p>}
+      {(!loading && !error) && <Title name={result.name || result.title} />}
+    </div>
   )
 }
