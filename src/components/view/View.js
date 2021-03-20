2fdebe48f3f7c478cc1e98514f69ca13a8838635
Bernadett Varga <detty.v@gmail.com>
2021-03-20 19:38:37 +0100

    Add dynamic detail rendering in View component

--- src/components/view/View.js
+++ src/components/view/View.js
@@ -38,7 +38,88 @@ export default function View (props) {
     <div>
       {(loading) && <p>Loading...</p>}
       {(!loading && error) && <p>Oops, an error occured.</p>}
-      {(!loading && !error) && <Title name={result.name || result.title} />}
+      {(!loading && !error) &&
+        <div>
+          <Title name={result.name || result.title} />
+          {renderDetails()}
+        </div>}
     </div>
   )
+
+  // ***********************
+
+  function renderDetails () {
+    const config = {
+      planets: [
+        'rotation_period',
+        'orbital_period',
+        'diameter',
+        'climate',
+        'gravity',
+        'terrain',
+        'surface_water',
+        'population'
+      ],
+      starships: [
+        'model',
+        'manufacturer',
+        'cost_in_credits',
+        'length',
+        'max_atmosphering_speed',
+        'crew',
+        'passengers',
+        'cargo_capacity',
+        'consumables',
+        'hyperdrive_rating',
+        'MGLT',
+        'starship_class'
+      ],
+      vehicles: [
+        'model',
+        'manufacturer',
+        'cost_in_credits',
+        'length',
+        'max_atmosphering_speed',
+        'crew',
+        'passengers',
+        'cargo_capacity',
+        'consumables',
+        'vehicle_class'
+      ],
+      people: [
+        'height',
+        'mass',
+        'hair_color',
+        'skin_color',
+        'eye_color',
+        'birth_year',
+        'gender'
+      ],
+      films: [
+        'episode_id',
+        'opening_crawl',
+        'director',
+        'producer',
+        'release_date'
+      ],
+      species: [
+        'classification',
+        'designation',
+        'average_height',
+        'average_lifespan',
+        'hair_colors',
+        'skin_colors',
+        'eye_colors',
+        'language'
+      ]
+    }
+
+    return config[entity].map((item) => render(item))
+
+    // *********************************
+
+    function render (item) {
+      return <p>{item}: {result[item]}</p>
+    }
+  }
 }
