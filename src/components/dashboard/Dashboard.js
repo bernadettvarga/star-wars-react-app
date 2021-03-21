e900947ace5098ecc4d0cbb3c2b6c6e94759bb4c
Bernadett Varga <detty.v@gmail.com>
2021-03-21 17:09:21 +0100

    Add content to Dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import img from '../../img/yoda.png'
 
 export default function Dashboard () {
   return (
-    <>
-      <div>Lorem Ipsum</div>
-    </>
+    <div className='dashboard'>
+      <img src={img} />
+      <p className='dashboard__quote'>
+        "To say <b>lorem ipsum</b>,<br />I must."<span className='dashboard__author'> - Yoda</span>
+      </p>
+    </div>
   )
 }
